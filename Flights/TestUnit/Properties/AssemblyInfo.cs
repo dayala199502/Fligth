cc92c1bb506aeec1ccaabf73ed87adea0f226087
Daniel Armando Ayala <dayalape@gmail.com>
2023-04-21 22:46:28 -0500

    DEV_#001 Entrega Final Proyecto

---
+++ Flights/TestUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a08e13c-f024-4d34-8fa1-d8b2fc5151df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Flights/TestUnit/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IFlightsInterfaces flightsInterfaces = new FlightsServices();
+            Assert.IsNotNull(flightsInterfaces.GetFligths());
+        }
+        [TestMethod]
+        public void TestMethod()
+        {
+            IFlightsInterfaces flightsInterfaces = new FlightsServices();
+            Assert.IsNotNull(flightsInterfaces.GetFligthsSearch(new Business.Models.JourneyModels()
+            {
+                Origin = "MZL",
+                Destination = "BCN"
+            }));
+
+        }
+    }
+}
